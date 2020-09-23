438f110f8df705e0ea241c8ef38b42c6a8d8a600
batophobia <godrox3@yahoo.com>
2020-09-22 22:42:11 -0500

    Initial commit

---
+++ Assets/Scripts/DriveController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DriveController : MonoBehaviour {
+  public float torque = 200f;
+  public float maxAngle = 30;
+
+  Wheel[] colliders;
+
+
+  void Start() {
+    colliders = GetComponentsInChildren<Wheel>();
+  }
+
+  void Update() {
+    float acc = Input.GetAxis("Vertical");
+    float turn = Input.GetAxis("Horizontal");
+
+    foreach(Wheel w in colliders){
+      if (w.isDrive) {
+        Go(w, acc);
+      } if (w.isSteer) {
+        Turn(w, turn);
+      }
+      w.UpdateModel();
+    }
+  }
+
+  void Go(Wheel w, float acc) {
+    acc = Mathf.Clamp(acc, -1, 1);
+    float thrust = acc * torque;
+    w.collider.motorTorque = thrust;
+  }
+
+  void Turn(Wheel w, float turn) {
+    turn = Mathf.Clamp(turn, -1, 1) * maxAngle;
+    w.collider.steerAngle = turn;
+  }
+}
---
+++ Assets/Scripts/Wheel.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Wheel : MonoBehaviour {
+  public bool isSteer = true;
+  public bool isDrive = true;
+  public GameObject model;
+  public WheelCollider collider;
+
+  private void Start() {
+    collider = GetComponent<WheelCollider>();
+  }
+
+  public void UpdateModel() {
+    Quaternion quat;
+    Vector3 pos;
+    collider.GetWorldPose(out pos, out quat);
+    model.transform.position = pos;
+    model.transform.rotation = quat;
+  }
+}
