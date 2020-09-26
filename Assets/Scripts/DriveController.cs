ee626fa1ec36e72e28e8fc31866701f85ddd54ce
batophobia <godrox3@yahoo.com>
2020-09-26 02:43:38 -0500

    Slightly better car driving

--- Assets/Scripts/DriveController.cs
+++ Assets/Scripts/DriveController.cs
@@ -1,40 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DriveController : MonoBehaviour {
-  public float torque = 200f;
-  public float maxAngle = 30;
+  public float torque = 500f;
+  public float maxAngle = 10f;
+  public float maxRPM = 200f;
+
+  public TextMeshProUGUI speed;
+  public Transform CoG;
 
   Wheel[] colliders;
-
+  Wheel driveWheel;
 
   void Start() {
     colliders = GetComponentsInChildren<Wheel>();
+    GetComponent<Rigidbody>().centerOfMass = CoG.localPosition;
+    driveWheel = getDrivingWheel();
   }
 
-  void Update() {
+  void FixedUpdate() {
     float acc = Input.GetAxis("Vertical");
     float turn = Input.GetAxis("Horizontal");
 
-    foreach(Wheel w in colliders){
+    foreach (Wheel w in colliders) {
       if (w.isDrive) {
         Go(w, acc);
-      } if (w.isSteer) {
+      }
+      if (w.isSteer) {
         Turn(w, turn);
       }
       w.UpdateModel();
     }
+    speed.text = getSpeed().ToString() + " km/h";
   }
 
   void Go(Wheel w, float acc) {
     acc = Mathf.Clamp(acc, -1, 1);
     float thrust = acc * torque;
-    w.collider.motorTorque = thrust;
+    if (Mathf.Abs(w.collider.rpm) < maxRPM)
+      w.collider.motorTorque = thrust;
+    else
+      w.collider.motorTorque = 0;
   }
 
   void Turn(Wheel w, float turn) {
     turn = Mathf.Clamp(turn, -1, 1) * maxAngle;
     w.collider.steerAngle = turn;
+  }
+
+  int getSpeed() {
+    if (driveWheel != null) {
+      return Mathf.RoundToInt(
+        driveWheel.collider.radius * Mathf.PI * driveWheel.collider.rpm // meters per minute
+        * 60 / 1000 // kilometers per hour
+      );
+    }
+    return 0;
+  }
+
+  Wheel getDrivingWheel() {
+    foreach (Wheel w in colliders) {
+      if (w.isDrive) {
+        return w;
+      }
+    }
+    return null;
   }
 }
